fdf38bb84d020c4b6b70bb1a2c8c0a4d7bb3693f
Norman Peitek <norman.peitek@gmail.com>
2018-03-22 09:32:31 +0100

    add graphs based on python input

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ import response_times_condition_boxplot as response_times
 
 response_times.draw_response_times_plot('ResponseTimeExport.csv', 'response_times.png')
 
-et_spat_err.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorExport.csv', 'eyetrack_spatial_error.png')
-et_spat_err.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorExportWeighted.csv', 'eyetrack_spatial_error_weighted.png')
+# Ogama
+et_spat_err.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorExport.csv', 'eyetrack_spatial_error_condition_ogama_raw.png')
+et_spat_err.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorExportWeighted.csv', 'eyetrack_spatial_error_condition_ogama_weighted.png')
 
-et_part.draw_eyetrack_spatial_error_plot('FixationRestAnalysisPartV2.csv', 'eyetrack_spatial_error_part_weighted.png')
+et_part.draw_eyetrack_spatial_error_plot('FixationRestAnalysisPartV2.csv', 'eyetrack_spatial_error_participants_ogama_weighted.png')
 
-et_part.draw_eyetrack_spatial_error_plot('FixationRestAnalysisPartV2.csv', 'eyetrack_spatial_error_part_weighted_indiv_x.png', True, False, 'x')
-et_part.draw_eyetrack_spatial_error_plot('FixationRestAnalysisPartV2.csv', 'eyetrack_spatial_error_part_weighted_indiv_y.png', True, False, 'y')
+et_part.draw_eyetrack_spatial_error_plot('FixationRestAnalysisPartV2.csv', 'eyetrack_spatial_error_participants_ogama_weighted_indiv_x.png', True, False, 'x')
+et_part.draw_eyetrack_spatial_error_plot('FixationRestAnalysisPartV2.csv', 'eyetrack_spatial_error_participants_ogama_weighted_indiv_y.png', True, False, 'y')
+
+# Python
+et_spat_err.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorPython.csv', 'eyetrack_spatial_error_condition_python_raw.png')
+et_spat_err.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorPythonWeighted.csv', 'eyetrack_spatial_error_condition_python_weighted.png')
+
+et_part.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorParticipantPython.csv', 'eyetrack_spatial_error_participants_python_raw.png')
+et_part.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorParticipantPython.csv', 'eyetrack_spatial_error_participants_python_raw_indiv_x.png', True, False, 'x')
+et_part.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorParticipantPython.csv', 'eyetrack_spatial_error_participants_python_raw_indiv_y.png', True, False, 'y')
+
+et_part.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorParticipantPythonWeighted.csv', 'eyetrack_spatial_error_participants_python_weighted.png')
+et_part.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorParticipantPythonWeighted.csv', 'eyetrack_spatial_error_participants_python_weighted_indiv_x.png', True, False, 'x')
+et_part.draw_eyetrack_spatial_error_plot('EyetrackSpatialErrorParticipantPythonWeighted.csv', 'eyetrack_spatial_error_participants_python_weighted_indiv_y.png', True, False, 'y')
