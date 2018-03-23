0d92db029214277aabf81e5b9d1c55f70d6a0ea8
Norman Peitek <norman.peitek@gmail.com>
2018-03-23 11:53:06 +0100

    add script for AOI histogram graph

---
+++ eyetrack_aoi_histogram.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from scipy import stats
+from os import path
+
+INPUT_PATH = 'input/'
+OUTPUT_PATH = 'output/'
+
+
+def draw_plot(input_file, output_file):
+    # Initialize the matplotlib figure
+    sns.set_style("ticks")
+    f, ax = plt.subplots(figsize=(6, 5))
+
+    # Load the example car crash dataset
+    aoi_participant_data = pd.read_csv(path.join(INPUT_PATH, input_file), sep=';')
+
+    colors = sns.color_palette("RdBu_r", 7)
+    x = np.linspace(0, 10, 1000)
+
+    distplot = sns.distplot(aoi_participant_data.distance_y, vertical=True, color=colors[1])
+
+    plt.plot(x, x + 25, linestyle='--', color=colors[0])
+    plt.plot(x, x - 25, linestyle='--', color=colors[0])
+
+    plt.plot(x, x + 50, linestyle=':', color=colors[-1])
+    plt.plot(x, x - 50, linestyle=':', color=colors[-1])
+
+    ax.set(xlabel="Percentage of Fixations", ylabel="Vertical Distance from Center of Task Identifier")
+
+    # Add text labels
+    plt.text(0.009, 70, 'Above Identifier', fontsize=10)
+    plt.text(0.0095, -5, 'Identifier', fontsize=10, color=colors[0])
+    plt.text(0.009, -90, 'Below Identifier', fontsize=10)
+
+    sns.despine(left=True, bottom=True)
+    plt.tight_layout()
+
+    # save output as file, in a high resolution
+    fig = distplot.get_figure()
+    fig.savefig(path.join(OUTPUT_PATH, output_file), dpi=300, transparent=False)
--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import eyetrack_spatial_participants as et_part
 import eyetrack_aoi_task_count as et_aoi_task_count
 import eyetrack_spatial_error_time as et_spat_err
 import eyetrack_aoi_participant_count
+import eyetrack_aoi_histogram
 import response_times_condition_boxplot as response_times
 
-#et_aoi_task_count.draw_eyetrack_spatial_error_plot('aoi_task_comprehension_time_count.csv', 'eyetrack_aoi_task_python_count.png')
-eyetrack_aoi_task_count_bar.draw_eyetrack_spatial_error_plot('aoi_task_comprehension_count_bar.csv', 'eyetrack_aoi_task_python_count_bar.png')
 
-#eyetrack_aoi_participant_count.draw_eyetrack_spatial_error_plot('aoi_task_participant_count.csv', 'eyetrack_aoi_participant_python_count.png')
+eyetrack_aoi_histogram.draw_plot('aoi_histogram_count.csv', 'eyetrack_aoi_histogram_count.png')
+eyetrack_aoi_histogram.draw_plot('aoi_histogram_time.csv', 'eyetrack_aoi_histogram_time.png')
 
 if False:
+    # AOI Analysis
+    et_aoi_task_count.draw_eyetrack_spatial_error_plot('aoi_task_comprehension_time_count.csv', 'eyetrack_aoi_task_python_count.png')
+    eyetrack_aoi_task_count_bar.draw_eyetrack_spatial_error_plot('aoi_task_comprehension_count_bar.csv', 'eyetrack_aoi_task_python_count_bar.png')
+    eyetrack_aoi_participant_count.draw_eyetrack_spatial_error_plot('aoi_task_participant_count.csv', 'eyetrack_aoi_participant_python_count.png')
+
+    # Response Time
     response_times.draw_response_times_plot('ResponseTimeExport.csv', 'response_times.png')
 
     # Ogama
